e2cbb900bba3f10b6ec3bb134d1656073d011337
Soo <109068985+thatgirls00@users.noreply.github.com>
2025-06-11 23:58:28 +0900

    feat: 캐싱 적용 및 자동 캐시 삭제 로직 추가

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import time
-import requests
-from dotenv import load_dotenv
 
 from crawler.student_meal import run_student_meal
 from crawler.faculty_meal import run_faculty_meal
@@ -10,35 +8,7 @@ from crawler.academic_notice import run_academic_notice
 from crawler.hankyong_notice import run_hankyong_notice
 from crawler.academic_schedule import run_academic_schedule
 
-def clear_cache():
-    print("\n✅ 캐시 삭제 요청 시작")
-
-    endpoints = [
-        "academic-notices",
-        "academic-schedule",
-        "student-meals",
-        "faculty-meals",
-        "dorm-meals",
-        "hankyong-notices",
-        "scholarship-notices"
-    ]
-
-    server_url = "http://211.188.57.74:8080"
-
-    for endpoint in endpoints:
-        try:
-            url = f"{server_url}/api/{endpoint}/clear-cache"
-            response = requests.post(url)
-            if response.status_code == 200:
-                print(f"✅ {endpoint} 캐시 삭제 성공")
-            else:
-                print(f"⚠️ {endpoint} 캐시 삭제 실패 - Status: {response.status_code}")
-        except Exception as e:
-            print(f"❌ {endpoint} 캐시 삭제 오류: {e}")
-
 def main():
-    load_dotenv(dotenv_path="/root/hknu_scraper/.env")  # .env 파일 로딩
-
     jobs = [
         ("학생식당", run_student_meal),
         ("교직원식당", run_faculty_meal),
@@ -48,7 +18,7 @@ def main():
         ("한경공지", run_hankyong_notice),
         ("학사일정", run_academic_schedule)
     ]
-    # 크롤링 실행
+
     for name, job in jobs:
         try:
             print(f"\n✅ [{name}] 실행 시작")
@@ -56,9 +26,6 @@ def main():
             time.sleep(5)
         except Exception as e:
             print(f"❌ [{name}] 오류 발생: {e}")
-
-    # 캐시 삭제 요청
-    clear_cache()
 
 if __name__ == "__main__":
     main()
